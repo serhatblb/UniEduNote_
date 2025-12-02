275296e0be8563757b0808652d89957515b7b876
serhatblb <serhatblb23@gmail.com>
2025-12-03 00:00:05 +0300

    Veritabani Postgres yapildi

--- uniedunote/settings.py
+++ uniedunote/settings.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
 from datetime import timedelta
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -92,10 +93,11 @@ WSGI_APPLICATION = "uniedunote.wsgi.application"
 # VERİTABANI
 # ------------------------------------------------------------------
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.config(
+        # Bilgisayarında çalışırken bu dosya yolunu kullanır (SQLite)
+        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
+        conn_max_age=600
+    )
 }
 
 # ------------------------------------------------------------------
