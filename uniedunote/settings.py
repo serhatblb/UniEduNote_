3224ccd73299bb205184335dff25b112beed0992
serhatblb <serhatblb23@gmail.com>
2025-12-03 13:16:10 +0300

    Whitenoise hata vermesin diye Manifest kapatildi4

--- uniedunote/settings.py
+++ uniedunote/settings.py
@@ -134,7 +134,7 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
     },
 }
 
