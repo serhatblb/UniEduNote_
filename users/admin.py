49fa43cf65bc3e9d7c992660e42ae37019169eec
serhatblb <serhatblb23@gmail.com>
2025-12-15 23:56:30 +0300

    Admin panelindeki points hatasi duzeltildi

--- users/admin.py
+++ users/admin.py
@@ -1,9 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import User
 
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'email', 'university', 'points', 'is_active', 'is_staff', 'date_joined')
-    search_fields = ('username', 'email', 'university')
-    list_filter = ('is_active', 'is_staff')
 
+class CustomUserAdmin(UserAdmin):
+    model = User
+    # Admin panelinde görünecek sütunlar
+    list_display = ['username', 'email', 'university', 'rank', 'is_staff']
+
+    # Hangi alanlara göre link verileceği
+    list_display_links = ['username', 'email']
+
+    # Düzenleme sayfasındaki alanlar (fieldsets)
+    fieldsets = UserAdmin.fieldsets + (
+        ('Ekstra Bilgiler', {'fields': ('university', 'avatar')}),
+    )
+
+    # Yeni kullanıcı ekleme sayfasındaki alanlar
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Ekstra Bilgiler', {'fields': ('university', 'avatar')}),
+    )
+
+
+admin.site.register(User, CustomUserAdmin)
