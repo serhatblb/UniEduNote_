09dbce5b04b27d7c9b8cfd01dce2036f77b97d1f
serhatblb <serhatblb23@gmail.com>
2026-01-05 00:15:17 +0300

    filtre alanı dzuenlendi

--- static/js/academic-hierarchy.js
+++ static/js/academic-hierarchy.js
@@ -105,7 +105,7 @@ class AcademicHierarchySelector {
                     return "Aranıyor...";
                 }
             },
-            allowClear: true
+            allowClear: false  // X işaretini kaldır
         });
     }
     
